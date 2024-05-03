4a77fea5eff4bcb8348796e3e143955a38c40fad
Emil Stenberg <esandin@gmail.com>
2024-05-03 21:16:25 +0200

    print cos and sin values using scipy

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 import numpy as np
-a = [1,1.6, 0]
+import scipy.fft as scipy_fft
+
+
+a = [1, 1.6, 0 ]
 # a = np.mgrid[:3, :3, :3][0]
 f = np.fft.fftn(a )
 
+
+print('cos:', scipy_fft.dct(a))
+print('sin:', scipy_fft.idst(a))
+print()
+ift = np.fft.ifftn(f)
+print()
+# print(f.complex)
 print("Original values:\n",a)
 print()
 print("Fouriere transform: \n",f)
+print("Inverse fft: \n", ift)
 
