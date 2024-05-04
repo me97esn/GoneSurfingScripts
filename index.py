c4655e0fd69d2e8dd3196a8443437e2a2830a2dd
Emil Stenberg <esandin@gmail.com>
2024-05-04 21:03:33 +0200

    created a function to calc ifft for one specific position

--- index.py
+++ index.py
@@ -4,7 +4,18 @@ a = np.array([1, 1.6, 0])
 n = len(a)
 print(a)
 b = np.fft.fft(a)
-print(b)
+
+def ifft(t):
+    ""
+    result = 0.0j
+    for k in range(n):
+        result = result + b[k]*np.exp(2*np.pi*1j*k*t/n)/n
+    return result
+
+print(ifft(0))
+print(ifft(1))
+print(ifft(2))
+
 # fill an array with zero imaginary numbers
 c = np.linspace(0, n-1, num=n)*1.0j
 for t in range(n):
