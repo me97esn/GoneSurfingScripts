b63321c35f5379996118d2cd5d25a65d3462279c
Emil Stenberg <esandin@gmail.com>
2024-05-04 21:05:03 +0200

    removed the imaginary part of the result

--- index.py
+++ index.py
@@ -10,17 +10,17 @@ def ifft(t):
     result = 0.0j
     for k in range(n):
         result = result + b[k]*np.exp(2*np.pi*1j*k*t/n)/n
-    return result
+    return result.real
 
 print(ifft(0))
 print(ifft(1))
 print(ifft(2))
 
 # fill an array with zero imaginary numbers
-c = np.linspace(0, n-1, num=n)*1.0j
-for t in range(n):
-    c[t] = 0.0
-    for k in range(n):
-        c[t] = c[t] + b[k]*np.exp(2*np.pi*1j*k*t/n)/n
-print(c)
+# c = np.linspace(0, n-1, num=n)*1.0j
+# for t in range(n):
+#     c[t] = 0.0
+#     for k in range(n):
+#         c[t] = c[t] + b[k]*np.exp(2*np.pi*1j*k*t/n)/n
+# print(c)
 
