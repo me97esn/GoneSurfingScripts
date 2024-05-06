bb4766136d0e0cfd2ef17d58767865ae20bea599
Emil Stenberg <esandin@gmail.com>
2024-05-06 16:09:25 +0200

    pass lenx and leny parameters to function

--- index.py
+++ index.py
@@ -5,15 +5,17 @@ print(a)
 b = np.fft.fft(a)
 
 
-def ifft2(x, y, fourierCoefficients):
+def ifft2(x, y, fourierCoefficients, lenX, lenY):
     ""
     result = 0.0j
     M = len(fourierCoefficients)
     N = len(fourierCoefficients[0])
+    lenX = M 
+    lenY = N
 
     for m in range(M):
         for n in range(N):
-            result = result + fourierCoefficients[m][n]*np.exp(2*np.pi*1j*(m*x/M + n*y/N))/(M*N)
+            result = result + fourierCoefficients[m][n]*np.exp(2*np.pi*1j*(m*x/lenX + n*y/lenY))/(lenX*lenY)
     return result.real
 
 
@@ -48,11 +50,11 @@ print("Original 2d array\n",random_2darray)
 # print("Fouriere transformed 2d array:\n",b2)
 # print(ifft2(1, 1, b2))
 print('-----------------')
-print("0,:" ,ifft2(0, 0, b2), ", should be: ", random_2darray[0][0])
-print("8,6:" ,ifft2(8, 6, b2), ", should be: ", random_2darray[8][6])
-print("9,5:" ,ifft2(9, 5, b2), ", should be: ", random_2darray[9][5])
+print("0,:" ,ifft2(0, 0, b2,random_2darray[0].size, random_2darray.size), ", should be: ", random_2darray[0][0])
+print("8,6:" ,ifft2(8, 6, b2,random_2darray[0].size, random_2darray.size), ", should be: ", random_2darray[8][6])
+print("9,5:" ,ifft2(9, 5, b2,random_2darray[0].size, random_2darray.size), ", should be: ", random_2darray[9][5])
 print()
-result = [[(ifft2(i, j, b2)) for i in range(random_2darray[0].size )] for j in range(random_2darray.size)]
+# result = [[(ifft2(i, j, b2)) for i in range(random_2darray[0].size )] for j in range(random_2darray.size)]
 
 
 # print(result)
