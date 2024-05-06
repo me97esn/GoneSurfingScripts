1c14814ddcf430dbf2345b194f590b21f2a84bc2
Emil <esandin@gmail.com>
2024-05-06 11:45:02 +0200

    wip: 2d ifft

--- index.py
+++ index.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 a = np.array([1, 1.6, 0])
-n = len(a)
 print(a)
 b = np.fft.fft(a)
 
 
-def ifft2(m, n, fourierCoefficients):
+def ifft2(x, y, fourierCoefficients):
     ""
     result = 0.0j
-    for m in range(n):
-        for n in range(n):
-            result = result + fourierCoefficients[k]*np.exp(2*np.pi*1j*k*t/n)/n
+    M = len(fourierCoefficients)
+    N = len(fourierCoefficients[0])
+
+    for m in range(M):
+        for n in range(N):
+            result = result + fourierCoefficients[m][n]*np.exp(2*np.pi*1j*(m*x/M))/N
     return result.real
 
 
 def ifft(t, fourierCoefficients):
     ""
+    n = len(fourierCoefficients)
     result = 0.0j
     for m in range(n):
         result = result + fourierCoefficients[m]*np.exp(2*np.pi*1j*m*t/n)/n
@@ -39,6 +42,7 @@ print(ifft(2, b))
 a2 = np.array([[0,1,0], [1, 1.1, 1.2], [1, 1, 1]])
 b2 = np.fft.fftn(a2)
 
-
-
-print(b2)
+print("-----------------")
+print("Original 2d array\n",a2)
+print("Fouriere transformed 2d array:\n",b2)
+print(ifft2(1, 1, b2))
