f762b51f7820e3b201c594df5161cbad06023a53
Emil Stenberg <esandin@gmail.com>
2024-05-06 22:29:41 +0200

    plot a graph

--- index.py
+++ index.py
@@ -1,19 +1,13 @@
 import numpy as np
-
-a = np.array([1, 1.6, 0])
-print(a)
-b = np.fft.fft(a)
-
+import matplotlib.pyplot as plt 
 
 def ifft2(x, y, fourierCoefficients, lenX, lenY):
     ""
     result = 0.0j
-    # M = len(fourierCoefficients)
-    # N = len(fourierCoefficients[0])
 
     for m in range(lenX):
         for n in range(lenY):
-            # if m  <= 6:
+            # if m  <= 8:
                 result = result + fourierCoefficients[m][n]*np.exp(2*np.pi*1j*(m*x/lenX + n*y/lenY))/(lenX*lenY)
     return result.real
 
@@ -41,22 +35,34 @@ def ifft(t, fourierCoefficients):
 # print(c)
 
 random_2darray = np.random.randint(5, size=(10, 10))
+# print(random_2darray)
 # a2 = np.array([[0,1,0], [1, 1.1, 1.2], [1, 1, 1]])
-b2 = np.fft.fftn(random_2darray)
+# b2 = np.fft.fftn(random_2darray)
 
-print("-----------------")
-print("Original 2d array\n",random_2darray)
-# print("Fouriere transformed 2d array:\n",b2)
-# print(ifft2(1, 1, b2))
-print('-----------------')
-print(random_2darray.shape[0])
-print("0,:" ,ifft2(0, 0, b2,random_2darray.shape[0], random_2darray.shape[1]), ", should be: ", random_2darray[0][0])
-print("8,6:" ,ifft2(8, 6, b2,random_2darray.shape[0], random_2darray.shape[1]), ", should be: ", random_2darray[8][6])
-print("9,5:" ,ifft2(9, 5, b2,random_2darray.shape[0], random_2darray.shape[1]), ", should be: ", random_2darray[9][5])
-print()
-# result = [[(ifft2(i, j, b2)) for i in range(random_2darray[0].size )] for j in range(random_2darray.size)]
+# print("-----------------")
+# print("Original 2d array\n",random_2darray)
+# # print("Fouriere transformed 2d array:\n",b2)
+# # print(ifft2(1, 1, b2))
+# print('-----------------')
+# print(random_2darray.shape[0])
+# print("0,:" ,ifft2(0, 0, b2,random_2darray.shape[0], random_2darray.shape[1]), ", should be: ", random_2darray[0][0])
+# print("8,6:" ,ifft2(8, 6, b2,random_2darray.shape[0], random_2darray.shape[1]), ", should be: ", random_2darray[8][6])
+# print("9,5:" ,ifft2(9, 5, b2,random_2darray.shape[0], random_2darray.shape[1]), ", should be: ", random_2darray[9][5])
+# print("9,9:" ,ifft2(9, 9, b2,random_2darray.shape[0], random_2darray.shape[1]), ", should be: ", random_2darray[9][9])
+# print()
+# result = [[(ifft2(i, j, b2, random_2darray.shape[0], random_2darray.shape[1])) for i in range(random_2darray[0].size )] for j in range(random_2darray.size)]
 
+random_1darray = np.random.randint(5, size=10)
 
+# len(result)
 # print(result)
 # for array in result:
 #     print(array)
+
+# plotting
+x = np.arange(1, len(random_1darray)+1)
+plt.title("Line graph") 
+plt.xlabel("X axis") 
+plt.ylabel("Y axis") 
+plt.plot(x, random_1darray, color ="red") 
+plt.show()
