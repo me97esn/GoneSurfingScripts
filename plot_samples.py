52b477c273ffd05814288525bcaadcd626876dbe
Emil Stenberg <esandin@gmail.com>
2024-05-19 00:43:46 +0200

    adjust the x and y plot after the data dimensions

--- plot_samples.py
+++ plot_samples.py
@@ -10,13 +10,12 @@ fig = plt.figure(figsize = (120,100))
 ax = plt.axes(projection='3d')
 # ax.set_xlim([xmin, xmax])
 ax.set_zlim([0, 50])
-# These dimensions have to match the dimensions of the data
 
-x = np.arange(0, 100, 1)
-y = np.arange(0, 100,1) 
+data = json.load(f)
+x = np.arange(0, len(data[0]), 1)
+y = np.arange(0, len(data[0][0]),1) 
 
 X, Y = np.meshgrid(x, y)
-data = json.load(f)
 Z = np.array(data[0])
 surf = ax.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
 
@@ -28,6 +27,6 @@ ax.set_zlabel('z', labelpad=200)
 for i in range(1, len(data)):
     Z = np.array(data[i])
     ax.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
-    plt.pause(0.1)
+    plt.pause(0.01)
 
 plt.show()
