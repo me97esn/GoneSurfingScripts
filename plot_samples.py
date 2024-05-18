f7dc622e540207c23520abda5c47852c09a904ed
Emil Stenberg <esandin@gmail.com>
2024-05-18 23:51:44 +0200

    animate the wave

--- plot_samples.py
+++ plot_samples.py
@@ -2,6 +2,8 @@ import numpy as np
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 import json
+import matplotlib.animation as animation
+
 f = open('samples.json')
 
 fig = plt.figure(figsize = (120,100))
@@ -14,19 +16,18 @@ x = np.arange(0, 100, 1)
 y = np.arange(0, 100,1) 
 
 X, Y = np.meshgrid(x, y)
-data_first_frame = json.load(f)[0]
-# print("num of samples x: ", len(data_first_frame[0]))
-# print(X)
-# for x in X:
-#     print(x)
-# Z = np.sin(X)*np.cos(Y)
-Z = np.array(data_first_frame)
-print("Z:",Z)
+data = json.load(f)
+Z = np.array(data[0])
 surf = ax.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
 
 # Set axes label
 ax.set_xlabel('x', labelpad=20)
 ax.set_ylabel('y', labelpad=20)
 ax.set_zlabel('z', labelpad=200)
+
+for i in range(1, len(data)):
+    Z = np.array(data[i])
+    ax.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
+    plt.pause(0.1)
 
 plt.show()
