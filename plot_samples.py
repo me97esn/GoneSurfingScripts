99016e699a5309c9ff9b570262f45b97d8cde6e7
Emil Stenberg <esandin@gmail.com>
2024-05-19 16:47:30 +0200

    display all of the ocean

--- plot_samples.py
+++ plot_samples.py
@@ -6,12 +6,11 @@ import matplotlib.animation as animation
 
 f = open('wave_samples.json')
 
-fig = plt.figure(figsize = (120,100))
+data = json.load(f)
+fig = plt.figure(figsize = (len(data[0][0]),len(data[0])))
 ax = plt.axes(projection='3d')
 # ax.set_xlim([xmin, xmax])
-ax.set_zlim([0, 50])
 
-data = json.load(f)
 x = np.arange(0, len(data[0][0]), 1)
 y = np.arange(0, len(data[0]),1) 
 
@@ -27,7 +26,7 @@ ax.set_zlabel('z', labelpad=200)
 while True:
    for i in range(1, len(data)):
        ax.clear()
-       ax.set_zlim([0, 50])
+       ax.set_zlim([0, 100])
        Z = np.array(data[i])
        ax.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
        plt.pause(0.01)
