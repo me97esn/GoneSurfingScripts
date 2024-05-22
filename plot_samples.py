58e4a5b8a33d0c0baa1d1a4ce161f947c99eb32f
Emil Stenberg <esandin@gmail.com>
2024-05-22 08:51:17 +0200

    refactor

--- plot_samples.py
+++ plot_samples.py
@@ -12,14 +12,10 @@ f = open('wave_samples.json')
 
 data = json.load(f)
 
-# 3d plot of examples
 ax0 = fig.add_subplot(1, 2, 2, projection='3d')
-
-# 3d plot of the samples
-# set up the Axes for the first plot
-ax = fig.add_subplot(1, 2, 1, projection='3d')
+samples_3d_plot = fig.add_subplot(1, 2, 1, projection='3d')
  
-for axis in [ax, ax0]:
+for axis in [samples_3d_plot, ax0]:
     axis.set_zlim([0, 100])
     axis.set_ylim([0, 150])
     axis.set_xlim([0, 150])
@@ -29,25 +25,25 @@ y = np.arange(0, len(data[0]),1)
 
 X, Y = np.meshgrid(x, y)
 Z = np.array(data[0])
-surf = ax.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
+surf = samples_3d_plot.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
 
 # Set axes label
-ax.set_xlabel('x', labelpad=20)
-ax.set_ylabel('y', labelpad=20)
-ax.set_zlabel('z', labelpad=200)
+samples_3d_plot.set_xlabel('x', labelpad=20)
+samples_3d_plot.set_ylabel('y', labelpad=20)
+samples_3d_plot.set_zlabel('z', labelpad=200)
 start_frame = 752
 while True:
     for i in range(1, len(data)):
         # plot 1
-        ax.clear()
-        ax.set_xlabel('Frame: ' + str(i+start_frame))
+        samples_3d_plot.clear()
+        samples_3d_plot.set_xlabel('Frame: ' + str(i+start_frame))
 
         # get the 2d array of all of the samples for this frame
         Z = np.array(data[i])
-        ax.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
+        samples_3d_plot.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
 
         # plot 2
-        X0, Y0, Z0 = get_test_data(0.05)
+        ax0.clear()
         ax0.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
 
         plt.pause(0.01)
