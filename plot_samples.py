c01c1a7d52fe72ebf0ae6d21b30853f0db81d765
Emil Stenberg <esandin@gmail.com>
2024-05-23 22:32:08 +0200

    remove the clear of the plots, the animation doesnt flicker now (but crappy performance)

--- plot_samples.py
+++ plot_samples.py
@@ -23,6 +23,8 @@ def filter_frequencies(data):
 
 # set up a figure twice as wide as it is tall
 fig = plt.figure(figsize=plt.figaspect(0.5))
+# plt.rcParams["figure.figsize"] = [7.00, 3.50]
+# plt.rcParams["figure.autolayout"] = True
 
 f = open('wave_samples.json')
 
@@ -32,17 +34,19 @@ filtered_data = filter_frequencies(data)
 
 filtered_3d_plot = fig.add_subplot(1, 2, 2, projection='3d')
 samples_3d_plot = fig.add_subplot(1, 2, 1, projection='3d')
- 
+# print(dir(samples_3d_plot))
 for axis in [samples_3d_plot, filtered_3d_plot]:
-    axis.set_zlim([0, 100])
-    axis.set_ylim([0, 150])
-    axis.set_xlim([0, 150])
+    axis.set_zlim3d(0, 60)
+    axis.set_ylim3d(0, 150)
+    axis.set_xlim3d(0, 150)
+    # axis.set_aspect("equal")
 
 x = np.arange(0, len(data[0][0]), 1)
 y = np.arange(0, len(data[0]),1)
 
 X, Y = np.meshgrid(x, y)
 
+plt.show(block=False)
 # Set axes label
 samples_3d_plot.set_xlabel('x', labelpad=20)
 samples_3d_plot.set_ylabel('y', labelpad=20)
@@ -52,7 +56,7 @@ while True:
     for i in range(1, len(data)):
         # plot 1, samples
 
-        samples_3d_plot.clear()
+        # samples_3d_plot.clear()
         samples_3d_plot.set_xlabel('Frame: ' + str(i+start_frame))
 
         # get the 2d array of all of the samples for this frame
@@ -60,7 +64,7 @@ while True:
         samples_3d_plot.plot_surface(X, Y, Z, cmap = plt.cm.cividis)
 
         # plot 2, fft
-        filtered_3d_plot.clear()
+        # filtered_3d_plot.clf()
         Z2 = np.array(filtered_data[i])
         # filtered_3d_plot.plot_wireframe(X, Y, Z2, rstride=10, cstride=10)
         filtered_3d_plot.plot_surface(X, Y, Z2, cmap = plt.cm.cividis)
@@ -76,4 +80,3 @@ while True:
 #     else:
 #         filtered_frequencies.append(0)
 
-# plt.show()
