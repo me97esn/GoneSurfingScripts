82f203a1482f71f53902bce8462a2a2a6580476f
Emil Stenberg <esandin@gmail.com>
2024-05-25 22:40:56 +0200

    set axis limits after clear

--- plot_samples.py
+++ plot_samples.py
@@ -35,10 +35,6 @@ filtered_data = filter_frequencies(data)
 filtered_3d_plot = fig.add_subplot(1, 2, 2, projection='3d')
 samples_3d_plot = fig.add_subplot(1, 2, 1, projection='3d')
 # print(dir(samples_3d_plot))
-for axis in [samples_3d_plot, filtered_3d_plot]:
-    axis.set_zlim3d(0, 60)
-    axis.set_ylim3d(0, 150)
-    axis.set_xlim3d(0, 150)
     # axis.set_aspect("equal")
 
 x = np.arange(0, len(data[0][0]), 1)
@@ -56,7 +52,14 @@ while True:
     for i in range(1, len(data)):
         # plot 1, samples
 
-        # samples_3d_plot.clear()
+        samples_3d_plot.clear()
+        filtered_3d_plot.clear()
+
+        for axis in [samples_3d_plot, filtered_3d_plot]:
+            axis.set_zlim3d(0, 60)
+            axis.set_ylim3d(0, 150)
+            axis.set_xlim3d(0, 150)
+
         samples_3d_plot.set_xlabel('Frame: ' + str(i+start_frame))
 
         # get the 2d array of all of the samples for this frame
